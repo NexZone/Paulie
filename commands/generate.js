134fa23c551fca504b161cace12fa7e7815b9aad
Sander Enger <nexzone03@hotmail.com>
2022-01-19 01:59:13 +0100

    feat: add command "generate"

---
+++ commands/generate.js
@@ -0,0 +1,11 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+
+export const generate = {
+    data: new SlashCommandBuilder()
+        .setName('generate')
+        .setDescription('Generates a random number between 0 and 100'),
+    async execute(interaction) {
+            let randomNumber = String(Math.floor(Math.random() * 100));
+            await interaction.reply(randomNumber);
+    }
+}
